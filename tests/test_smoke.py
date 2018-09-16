bee13c264a7fe0ca040726d00cee3ec8159faab2
Alexander Liao <alexander_liao@outlook.com>
2018-09-16 19:23:49 -0400

    jigsaw

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,12 +4,16 @@ import cv2
 
 from gaps import image_helpers
 from gaps.genetic_algorithm import GeneticAlgorithm
+from PIL import Image
+from keras.preprocessing.image import load_img
 
-GENERATIONS = 3
-POPULATION = 100
-PIECE_SIZE = 128
 
-image = cv2.imread("images/baboon.jpg")
+GENERATIONS = 20
+POPULATION = 300
+PIECE_SIZE = 101
+
+image = np.array(cv2.imread("../train.png"))
+print(image.shape)
 
 @pytest.fixture
 def puzzle():
@@ -20,5 +24,10 @@ def puzzle():
 def test_puzzle_solver(puzzle):
     algorithm = GeneticAlgorithm(puzzle, PIECE_SIZE, POPULATION, GENERATIONS)
     solution = algorithm.start_evolution(verbose=False)
-
+    A=solution.to_image()
+    print(type(A))
+    im = Image.fromarray(A)
+    im.show()
     assert np.array_equal(image, solution.to_image())
+
+test_puzzle_solver(image)
